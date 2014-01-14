24b450eadbd0a9f29809450343ffb54d65ea61de
damian <damian@tardis.(none)>
2014-01-14 22:36:31 +0800

    Updated scripts to properly do for loops.

--- two_car_lanes_virtual.sh
+++ two_car_lanes_virtual.sh
@@ -2,17 +2,17 @@
 
 #Script to be used with traffic_simulation.
 
-export TRIALS=50
-export TIMESTEPS=3000
-export ROADLENGTH=50
-export REAL_LANES=4
-export VIRTUAL_LANES=1
-export LANE_CHANGE=1
+TRIALS=50
+TIMESTEPS=3000
+ROADLENGTH=50
+REAL_LANES=4
+VIRTUAL_LANES=1
+LANE_CHANGE=1
 
 mkdir Two_Car_Lanes_Virtual
 cd Two_Car_Lanes_Virtual
 
-for car_ratio in "seq 0 0.05 1";
+for i in `seq 0 0.05 1`;
 do
-	../traffic_simulation -c $car_ratio -T $TRIALS -t $TIMESTEPS -R $ROADLENGTH -r $REAL_LANES -v $VIRTUAL_LANES -L $LANE_CHANGE
+	../traffic_simulation -c $i -T $TRIALS -t $TIMESTEPS -R $ROADLENGTH -r $REAL_LANES -v $VIRTUAL_LANES -L $LANE_CHANGE
 done
