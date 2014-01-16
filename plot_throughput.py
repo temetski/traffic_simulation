c81771b0f496d2ea4f83ee6191f27e60473cbfac
Damian Dailisan <damian.dailisan@gmail.com>
2014-01-16 08:02:11 +0800

    renamed variance_throughput.py to plot_throughput.py. Supports headless file plotting, and bzipping hdf5 files.

--- plot_throughput.py
+++ plot_throughput.py
@@ -7,9 +7,13 @@ Created on Mon Jul 22 00:18:38 2013
 """
 import numpy as np
 import cPickle
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.lines as lines
 import matplotlib.pyplot as plt
+from subprocess import call
 import h5py
+
 
 REAL_LANES = 4
 ROADLENGTH = 50
@@ -32,24 +36,27 @@ def through(vehicles):
 
 def load(ratio, density):
     throughput = np.array([], dtype=np.int8)
+    filename = "CarRatio.%.2f_Density.%.2f.h5" % (ratio,density)
+    call(['bunzip2', filename+'.bz2'])
+    fid = h5py.File(filename, 'r')
     for t in xrange(TRIALS):
-#                bzfile = bz2.BZ2File("CarRatio.%.2f_Density.%.2f.h5.bz2" % (ratio,density))
-#                ucdata = bz2.decompress()
-        fid = h5py.h5f.open("CarRatio.%.2f_Density.%.2f.h5" % (ratio,density))
-        group = h5py.h5g.open(fid, "CarRatio::%.2f/Density::%.2f/" %
-                                    (ratio, density))
-        dset = h5py.h5d.open(group, "Trial::%04d" % (t+1))
-        rdata = np.zeros(dset.shape)
-        dset.read(h5py.h5s.ALL, h5py.h5s.ALL, rdata)
-       # throughput.append(rdata)
-        throughput = np.append(throughput, rdata)
+        group = "CarRatio::%.2f/Density::%.2f/" % (ratio, density)
+        trial = "Trial::%04d" % (t+1)
+        dset = fid[group+trial]
+        throughput = np.append(throughput, dset)
+
+    throughput = np.reshape(throughput, (TRIALS, dset.shape[0],
+                                         dset.shape[1],
+                                         dset.shape[2]))
+    fid.close()
+    call(["bzip2", "-6", filename])
     return throughput
 
 
 
-fluxes = np.zeros([9,19])
-variance = np.zeros([9,19, 2])
-for x, ratio in enumerate(np.arange(0.1,1,0.1)):
+fluxes = np.zeros([1,19])
+variance = np.zeros([1,19, 2])
+for x, ratio in enumerate(np.arange(1)):#0.1,1,0.1)):
     if ratio == 1:
         ratio = 1
     if ratio == 0:
@@ -73,11 +80,11 @@ for ydata, label, i in zip(fluxes, labels, range(len(labels))):
 #    plt.plot(np.arange(0.05,1,0.05), ydata, label=label, marker=marker)
     plt.errorbar(np.arange(0.05,1,0.05), ydata, yerr=np.abs(variance[i].T-ydata),label=label, marker=marker)
 #plt.legend(loc="upper left", bbox_to_anchor=(1,1))
-plt.legend()
+#plt.legend()
 plt.xlabel('road density', size=18)
 plt.ylabel('number of exiting vehicles', size=18)
-plt.ylim(0,2000)
-plt.xlim(0,1.2)
+#plt.ylim(0,2000)
+#plt.xlim(0,1.2)
 plt.title(r'Two Real lanes with adaptation', size=18)
 
 plt.savefig("throughput.png")
